3b679dfd2baa9171fe0d9cbc3ce34066d962f82d
griffin-walker <griffin.walker@mac.com>
2022-01-10 22:51:30 -0500

    Add my own resource, add logging

--- app/main.py
+++ app/main.py
@@ -2,6 +2,14 @@ from typing import Optional
 
 from fastapi import FastAPI
 
+import logging
+from logging.config import dictConfig
+from .config import LogConfig
+
+dictConfig(LogConfig().dict())
+logger = logging.getLogger("hello-fastapi")
+
+
 app = FastAPI()
 
 
@@ -13,3 +21,9 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/bags/{bag_id}")
+def get_bag(bag_id: int):
+    logger.info("Got a bag.")
+    return {"bag_id": bag_id}
